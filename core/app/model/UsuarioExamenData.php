2e1b19552e34856bf8043440904101c86993c2e3
databj <benjiable21@gmail.com>
2021-09-17 17:43:38 -0500

    retoques

--- core/app/model/UsuarioExamenData.php
+++ core/app/model/UsuarioExamenData.php
@@ -73,7 +73,7 @@ class UsuarioExamenData
 
 	public static function getByIdUser($id)
 	{
-		$sql = "select * from " . self::$tablename . " where id_usuario=$id";
+		$sql = "select * from " . self::$tablename . " where id_usuario=$id and estado=0";
 		$query = Executor::doit($sql);
 		return Model::one($query[0], new UsuarioExamenData());
 	}
